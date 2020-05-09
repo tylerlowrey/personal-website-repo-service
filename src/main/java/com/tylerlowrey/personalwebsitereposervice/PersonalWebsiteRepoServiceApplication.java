351a271ee4874308960ec955ac68268e544868bc
tylerlowrey <contact@tylerlowrey.com>
2020-05-09 12:47:48 -0400

    Initial commit

---
+++ src/main/java/com/tylerlowrey/personalwebsitereposervice/PersonalWebsiteRepoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tylerlowrey.personalwebsitereposervice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalWebsiteRepoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalWebsiteRepoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tylerlowrey/personalwebsitereposervice/PersonalWebsiteRepoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tylerlowrey.personalwebsitereposervice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWebsiteRepoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
